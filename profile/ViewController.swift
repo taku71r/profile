7843a4b39866981097f812bc64f83ff668ceb386
Takuroh Shimizu <his7716twd@gmail.com>
2020-05-14 11:43:54 +0900

    Initial Commit

---
+++ profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  profile
+//
+//  Created by 清水拓郎 on 2020/05/14.
+//  Copyright © 2020 清水拓郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
